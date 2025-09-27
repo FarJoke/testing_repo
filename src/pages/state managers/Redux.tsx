0cc7e7b6b3517402ed3daf04030402e0310fb246
Дубовой Олег <dubovoyo@inlinegroup-c.ru>
2025-09-27 18:33:40 +0300

    Fix

--- src/pages/state managers/Redux.tsx
+++ src/pages/state managers/Redux.tsx
@@ -1,4 +1,16 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
 export default function Redux (){
+
+    const counterSlice = createSlice({
+        name: "counter",
+        initialState: { value: 0 },
+        reducers: {
+            increment: (state) => { state.value += 1; },
+            decrement: (state) => { state.value -= 1; },
+        },
+    });
+
     return(
         <>
         Hello World!
